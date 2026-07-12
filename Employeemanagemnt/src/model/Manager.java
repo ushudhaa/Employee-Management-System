9c4756829f01e8b602983f206db082e59feb37f7
Ushudha Sanwa Limbu <ushudhasanwa@gmail.com>
2026-07-12 22:47:06 +0545

    Create manager model structure

--- Employeemanagemnt/src/model/Manager.java
+++ Employeemanagemnt/src/model/Manager.java
@@ -1,4 +1,16 @@
 package model;
 
-public class Manager {
+public class Manager extends Employee {
+
+    private String department;
+    private int teamSize;
+
+    public Manager(int ID, String Name, String Address, int Age,
+                   Double salary, String department, int teamSize) {
+
+        super(ID, Name, Address, Age, salary);
+
+        this.department = department;
+        this.teamSize = teamSize;
+    }
 }

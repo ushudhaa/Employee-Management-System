845e4957f0e800e414efef793ae78210a5876fbc
Ushudha Sanwa Limbu <ushudhasanwa@gmail.com>
2026-07-12 22:48:32 +0545

    Create salary model structure

--- Employeemanagemnt/src/model/Salary.java
+++ Employeemanagemnt/src/model/Salary.java
@@ -1,4 +1,12 @@
 package model;
 
 public class Salary {
+
+    private int employeeId;
+    private double basicSalary;
+
+    public Salary(int employeeId, double basicSalary) {
+        this.employeeId = employeeId;
+        this.basicSalary = basicSalary;
+    }
 }

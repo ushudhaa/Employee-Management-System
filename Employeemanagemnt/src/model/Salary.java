768db61f573e166caf0a8bdde9e5fc87f5a36a5d
Ushudha Sanwa Limbu <ushudhasanwa@gmail.com>
2026-07-12 22:48:55 +0545

    Add salary model accessors and string representation

--- Employeemanagemnt/src/model/Salary.java
+++ Employeemanagemnt/src/model/Salary.java
@@ -9,4 +9,27 @@ public class Salary {
         this.employeeId = employeeId;
         this.basicSalary = basicSalary;
     }
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getBasicSalary() {
+        return basicSalary;
+    }
+
+    public void setBasicSalary(double basicSalary) {
+        this.basicSalary = basicSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Salary{" +
+                "employeeId=" + employeeId +
+                ", basicSalary=" + basicSalary +
+                '}';
+    }
 }

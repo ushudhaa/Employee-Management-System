a7c1591ffcc7c4e1350502ed0126fb6628f1c098
Ushudha Sanwa Limbu <ushudhasanwa@gmail.com>
2026-07-14 22:29:20 +0545

    Add spacing utility method

--- Employeemanagemnt/src/util/EmployeeUtil.java
+++ Employeemanagemnt/src/util/EmployeeUtil.java
@@ -6,11 +6,16 @@ public class EmployeeUtil {
     public static void line() {
         System.out.println("------------------------------");
     }
+
     // Prints a title with separator lines
     public static void title(String title) {
-
         line();
         System.out.println(title);
         line();
+    }
+
+    // Prints a blank line
+    public static void space() {
+        System.out.println();
     }
 }

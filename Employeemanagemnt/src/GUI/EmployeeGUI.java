aa46235f6158b0577edafc316edd20d68faa3b00
Ushudha Sanwa Limbu <ushudhasanwa@gmail.com>
2026-07-19 23:26:37 +0545

    Java 8: Optional avoids returning/checking null when searching for an employee

--- Employeemanagemnt/src/GUI/EmployeeGUI.java
+++ Employeemanagemnt/src/GUI/EmployeeGUI.java
@@ -1,4 +1,23 @@
 package GUI;
 
+import model.Employee;
+import repository.EmployeeRepository;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
 public class EmployeeGUI {
-}
+    public static final EmployeeRepository repository = new EmployeeRepository();
+
+    public static void main(String[] args) {
+        new WelcomeFrame();
+    }
+    public static Optional<Employee> findById(int id) {
+        return repository.findAll().stream()
+                .filter(e -> e.getID() == id)
+                .findFirst();
+    }
+
